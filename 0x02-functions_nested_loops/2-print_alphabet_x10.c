d08a778ff8f22a5735989fdb8156fa60ca3f4521
Joyce-Masiga <joycemasiga@gmail.com>
2023-02-21 07:35:46 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+*print_alphabet_x10 - Make the alphabet print ten times
+*Description: Making the aplhabet print lower case ten times
+*Return: void
+*/
+
+void print_alphabet_x10(void)
+
+{
+char c;
+int i = 0;
+
+while (i <= 9)
+{
+
+for (c = 97; c < 123; c++)
+{
+_putchar(c);
+}
+_putchar('\n');i++;
+}
+}
