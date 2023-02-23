771715ebef797bf9a90ff241fc0d8de2b288fca0
Joyce-Masiga <joycemasiga@gmail.com>
2023-02-23 09:09:38 -0800

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,11 +3,12 @@
 /**
  * mul - Checks if a character is a digit
  * @x: The first number to multiply
+ * @y: The seconf number to multiply
  * Description: A function that multiplies two integers
  * Return: The result of multiplication between x param and y param
  */
 
 int mul(int x, int y)
 {
-return (x *y);
+return (x * y);
 }
