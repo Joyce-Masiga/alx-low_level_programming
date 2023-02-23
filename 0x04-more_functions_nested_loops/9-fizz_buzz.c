3933f1c43ccb3adfc95f0c99f8a9e1a51d67f3a5
Joyce-Masiga <joycemasiga@gmail.com>
2023-02-23 13:09:12 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,6 +11,7 @@ int main(void)
 
 {
 int x;
+
 for (x = 1; x <= 100; x++)
 {
 if ((x % 3 == 0) && (x % 5 == 0))
@@ -31,7 +32,7 @@ printf("%d", x);
 }
 if (x != 100)
 {
-printf("");
+printf(" ");
 }
 }
 printf("\n");
